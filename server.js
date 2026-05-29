e5a851ad34c7c6ff227c21245ebab895bc37e9fa
Leonardo <leonardomales080@gmail.com>
2026-05-29 10:56:55 -0500

    Primer commit limpio sin node_modules

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var app = express()
+
+app.get('/home',(req,res) => res.send("Hola Ciapfa"))
+app.post('/login',(req,res) => res.send("Bienvenido usuario"))
+app.patch('/actualizar',(req,res) => res.send("Usuario actualizado"))
+app.delete('/login',(req,res) => res.send("Bienvenido usuario"))
+
+app.listen(3000,()=>console.log("Escuchando en el puerto 3000"))
